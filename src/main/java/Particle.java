c6fffb110c3c64a22de61c1073e91238bdf28e36
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2020-08-09 22:05:56 -0300

    Particle changes

--- src/main/java/Particle.java
+++ src/main/java/Particle.java
@@ -1,4 +1,5 @@
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -6,6 +7,8 @@ public class Particle implements Comparable<Particle>{
     private int id;
     private double x;
     private double y;
+    private double vx;
+    private double vy;
     private double radius;
     private double property;
     private Set<Particle> neighbours;
@@ -13,13 +16,15 @@ public class Particle implements Comparable<Particle>{
     private int cellRow;
     private int cellColumn;
 
-    public Particle(int id, double x, double y, double radius, double property, int cellRow, int cellColumn) {
+    public Particle(int id, double x, double y, double vx, double vy, double radius, double property, int cellRow, int cellColumn) {
         this.id = id;
         this.x = x;
         this.y = y;
+        this.vx = vx;
+        this.vy = vy;
         this.radius = radius;
         this.property = property;
-        this.neighbours = new TreeSet<>();
+        this.neighbours = new HashSet<>();
         this.cellRow = cellRow;
         this.cellColumn = cellColumn;
     }
@@ -32,7 +37,7 @@ public class Particle implements Comparable<Particle>{
     }
 
     public int compareTo(Particle particle) {
-        return 0;
+        return id - particle.getId();
     }
 
     public int getId() {
@@ -45,6 +50,14 @@ public class Particle implements Comparable<Particle>{
 
     public double getY() {
         return y;
+    }
+
+    public double getVx() {
+        return vx;
+    }
+
+    public double getVy() {
+        return vy;
     }
 
     public double getRadius() {
@@ -100,9 +113,7 @@ public class Particle implements Comparable<Particle>{
         if (this == o) return true;
         if (o == null || this.getClass() != o.getClass()) return false;
         Particle particle = (Particle) o;
-        return this.id == particle.getId()
-                && this.x == particle.getX()
-                && this.y == particle.getY();
+        return this.id == particle.getId();
     }
 
     @Override
@@ -126,6 +137,14 @@ public class Particle implements Comparable<Particle>{
 
     public void setY(double y) {
         this.y = y;
+    }
+
+    public void setVx(double vx) {
+        this.vx = vx;
+    }
+
+    public void setVy(double vy) {
+        this.vy = vy;
     }
 
     public void setRadius(double radius) {
